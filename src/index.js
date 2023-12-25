5225bc00bd6170e4ac707004e1fa8afa3a19115a
15439 <1543922073@qq.com>
2023-12-25 14:52:48 +0800

    23

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import DragTimePicker from "./index.vue";
 DragTimePicker.install = (Vue) => {
   Vue.component(DragTimePicker.name, DragTimePicker);
 };
-console.log("test");
+
 export default DragTimePicker;
